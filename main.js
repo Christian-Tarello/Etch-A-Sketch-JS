ba46559f12b8fa585753943dd7923b6794907410
Christian Tarello <christian_tarello@outlook.com>
2022-09-29 19:47:47 -0500

    Update drawGrid function to accept a columns argument

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const container = document.querySelector(".flex-container");
 
-function drawGrid(container) {
-    for (let i = 0; i<(16*16); i++) {
+function drawGrid(container, columns = 16) {
+    const totalSquares = Math.pow(columns, 2);
+    const squarePercentage = 100/columns;
+
+    for (let i = 0; i<(totalSquares); i++) {
         const square = document.createElement("div");
-        square.classList.add("square");
-        square.style.flex =`1 1 ${100/16}%`;
+        square.style.flex =`1 1 ${squarePercentage}%`;
         container.appendChild(square);
     }
 }
 
-drawGrid(container);
+drawGrid(container, 16);
